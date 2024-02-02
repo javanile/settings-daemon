0057e73773347a2a9f6457bdd687af854ae8b60b
francescobianco <info.francescobianco@gmail.com>
2024-02-02 09:48:37 +0100

    First commit

--- src/commands/run.sh
+++ src/commands/run.sh
@@ -10,5 +10,12 @@ mkdir -p "${SETTINGS_DAEMON_BUILD}/packet"
 
 find "${SETTINGS_DAEMON_BUILD}" -type d -depth 1 | while read packet; do
     echo "Packet: ${packet}"
+    packet_dir=$(realpath "${packet}")
+    packet_file=${packet_dir}.zip
+    touch "${packet_dir}/test"
+    run_pwd=${PWD}
+    cd "${packet_dir}"
+    zip -o -r ${packet_file} .
+    cd "${run_pwd}"
 done
 }
